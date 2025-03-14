2862145c0a06bb6d5872b2bacd739b71a38719a7
pulpombo <152027060+pulpombo@users.noreply.github.com>
2025-03-14 12:10:34 +0100

    Kickstart

---
+++ code.js
@@ -0,0 +1,11 @@
+function AbrirMenú(){
+
+    document.getElementById("menú-celular").style.width = "100%"
+
+}
+
+function CerrarMenú(){
+
+    document.getElementById("menú-celular").style.width = "0%"
+
+}
